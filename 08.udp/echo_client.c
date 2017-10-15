b461da5f58bdf50aa40c4ddc72ad25da0549f483
LiuLang <gsushzhsosgsu@gmail.com>
2017-10-15 21:08:38 +0800

    udp: Support bind()

--- 08.udp/echo_client.c
+++ 08.udp/echo_client.c
@@ -5,37 +5,26 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <unistd.h>
 
 const uint16_t kServerPort = 9191;
 
 static void Echo(FILE* input, int sock_fd, struct sockaddr* server_addr,
                  socklen_t server_len) {
-  ssize_t n_send, n_recv;
+  ssize_t n_send = 0, n_recv = 0;
   const ssize_t kBufSize = 1024;
   char send_buf[kBufSize], recv_buf[kBufSize + 1];
-  socklen_t reply_len;
-  struct sockaddr* reply_addr = NULL;
+  int status = 0;
 
-  reply_addr = malloc(server_len);
-  if (reply_addr == NULL) {
-    perror("malloc()");
+  status  = connect(sock_fd, server_addr, server_len);
+  if (status == -1) {
+    perror("connect()");
     return;
   }
 
   while (fgets(send_buf, kBufSize, input) != NULL) {
-    n_send = sendto(sock_fd, send_buf, strlen(send_buf), 0, server_addr,
-                    server_len);
-    reply_len = server_len;
-    n_recv = recvfrom(sock_fd, recv_buf, kBufSize, 0, reply_addr, &reply_len);
-
-    if ((reply_len != server_len) ||
-        (memcmp(server_addr, reply_addr, reply_len) != 0)) {
-      char host_buf[128];
-      const char* host_ptr = inet_ntop(AF_INET, (const void*) server_addr,
-                                       host_buf, reply_len);
-      fprintf(stderr, "reply from %s (ignored)\n", host_ptr);
-      continue;
-    }
+    n_send = write(sock_fd, send_buf, strlen(send_buf));
+    n_recv = read(sock_fd, recv_buf, kBufSize);
 
     if (n_send != n_recv) {
       fprintf(stderr, "send: %d, recv: %d not match\n", n_send, n_recv);
